2423edf081ea6773015089c916638eee02d6af87
jaykim97 <colinjkim1997@gmail.com>
2021-04-26 14:40:56 -0400

    modified to add ascii art creator

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from asciiTrans import asciiTran
+import numpy
+import cv2
 
 app = Flask(__name__)
 
@@ -6,3 +9,17 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     return render_template('index.html')
+
+
+@app.route('/ascii', methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+        res = asciiTran(cv2.imdecode(numpy.fromstring(
+            request.files['file'].read(), numpy.uint8), cv2.IMREAD_UNCHANGED))
+        return render_template('ascii.html', img_src=res.decode('utf-8'))
+    elif request.method == 'GET':
+        return render_template('ascii.html')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1')
