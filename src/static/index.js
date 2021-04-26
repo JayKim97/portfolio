c78cae4ff4004e4f8b7a7914e9e4a75c1fc49db1
jaykim97 <colinjkim1997@gmail.com>
2021-04-26 01:14:05 -0400

    nav, and email script added

--- src/static/index.js
+++ src/static/index.js
@@ -6,8 +6,33 @@ function loadStart() {
 }
 
 function showEmail() {
-  var email = document.getElementById("emaillink");
-  email.style.display = "inline";
+  var email = document.getElementById("emaillink1");
+  if (email.style.display === "inline") {
+    email.style.animation = "animationgone 1s";
+    setTimeout(() => (email.style.display = "none"), 1000);
+  } else {
+    email.style.display = "inline";
+    email.style.animation = "animateleft 1s";
+  }
+}
+
+function showEmail2() {
+  var email = document.getElementById("emaillink2");
+  if (email.style.display === "inline") {
+    email.style.animation = "animationgone 1s";
+    setTimeout(() => (email.style.display = "none"), 1000);
+  } else {
+    email.style.display = "inline";
+    email.style.animation = "animateleft 1s";
+  }
+}
+
+function activateMenu(num) {
+  var childs = Array.from(document.getElementById("topnav").children);
+  childs.forEach((element) => {
+    element.classList.remove("active");
+  });
+  childs[num].classList.add("active");
 }
 
 document.onload = loadStart();
